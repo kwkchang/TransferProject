e9839c215c239489bcf7a39d9589c7b024b52315
Patrick <pwblais@icloud.com>
2017-04-20 10:17:18 -0400

    Main Menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    private Button startbtn;
+
+    @FXML
+    private Button exitbtn;
+
+    @FXML
+    void startOnAction(ActionEvent event) {
+        Stage primaryStage = (Stage) startbtn.getScene().getWindow();
+        primaryStage.setScene(new Scene(new Pane()));
+    }
+
+    @FXML
+    void exitOnAction(ActionEvent event) {
+        Platform.exit();
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,23 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1920, 1080));
+        Scene scene = new Scene(root, 400, 400);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
+
+
+
     }
 
+    public void map1(Parent root,Scene scene, Stage primaryStage) {
+        Scene map1 = new Scene(root, 1920, 1080);
+        primaryStage.setScene(map1);
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
         launch(args);
